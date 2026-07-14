82dbe9bd533eb046feaccf07945fabddbc8f4712
tricraft <cornefranscoise@gmail.com>
2026-07-14 18:22:26 +0200

    Allow flag game to start in any channel
    Removed strict ID check for starting the flag game.

--- commands/flag.js
+++ commands/flag.js
@@ -1,5 +1,4 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
-const config = require('../config.json');
 const messageEvent = require('../events/messageCreate.js');
 
 module.exports = {
@@ -9,16 +8,10 @@ module.exports = {
         .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
 
     async execute(interaction) {
-        const guildId = interaction.guild.id;
-        const serverConfig = Object.values(config.servers).find(s => s.guildId === guildId);
-
-        if (!serverConfig || interaction.channel.id !== serverConfig.vlaggen) {
-            return interaction.reply({ content: 'Je kunt dit spel alleen opstarten in het officiële vlaggen-kanaal!', ephemeral: true });
-        }
-
-        await interaction.reply({ content: '🚀 **Vlaggen-raadspel wordt nu opgestart...**', ephemeral: true });
+        // De strenge ID-check is nu weggehaald zodat het commando altijd luistert
+        await interaction.reply({ content: '🚀 **Vlaggen-raadspel wordt nu in dit kanaal opgestart...**', ephemeral: true });
         
-        // Roep de startfunctie aan uit het eventbestand
+        // Start direct de eerste vlaggenronde in de chat
         messageEvent.startNewFlagRound(interaction.channel);
     },
 };
